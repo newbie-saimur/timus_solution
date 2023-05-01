2d0c5cc30aaee768e5095e1256ad0e25948d8e1b
Md Saimur Rahman Robin <robin15-6206@s.diu.edu.bd>
2023-05-01 16:44:01 +0600

    Create 1197. Lonesome Knight.cpp

---
+++ 1197. Lonesome Knight.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define i_mn INT_MIN
+#define i_mx INT_MAX
+
+#define vi vector<int>
+#define vll vector<ll>
+#define vb vector<bool>
+#define pb push_back
+
+#define pii pair<int, int>
+#define vip vector<pair<int, int>>
+#define m_p make_pair
+#define mii map<int, int>
+
+#define ff first
+#define ss second
+#define in insert
+#define ub upper_bound
+#define lb lower_bound
+
+#define all(v) v.begin(), v.end()
+#define asc(v) sort(v.begin(), v.end())
+#define dsc(v) sort(v.rbegin(), v.rend())
+
+#define ign cin.ignore()
+#define getln(s) getline(cin, s)
+#define fli(n) for (int i = 0; i < n; i++)
+#define rfli(n) for (int i = n - 1; i >= 0; i--)
+#define flj(n) for (int j = 0; j < n; j++)
+
+#define eps (double)(1e-7)
+#define pi (double)(acos(-1))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * b) / __gcd(a, b)
+#define mod (ll)(1e9 + 7)
+
+#define case cout << "Case " << k << ": "
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define endl "\n"
+int k;
+
+//= = = = = = = = = = End Of Template = = = = = = = = = =
+
+int xx[] = {-2, 2, -2, 2, -1, 1, -1, 1};
+int yy[] = {-1, -1, 1, 1, -2, -2, 2, 2};
+
+bool inside_boundary(int x, int y)
+{
+    return x >= 0 && x < 8 && y >= 0 && y < 8;
+}
+
+void solve()
+{
+    string s;
+    cin >> s;
+    int a, b;
+    a = s[1] - '1';
+    b = s[0] - 'a';
+    int ans = 0;
+    for (int i = 0; i < 8; i++)
+        ans += inside_boundary(a+xx[i], b+yy[i]);
+    cout << ans << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ll test = 1;
+    cin >> test;
+    for (k = 1; k <= test; k++)
+    {
+        solve();
+    }
+    return 0;
+}
