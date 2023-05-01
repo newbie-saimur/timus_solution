61b8fcafb0c0c64190c7653c11f118ffe1d7cc24
Md Saimur Rahman Robin <robin15-6206@s.diu.edu.bd>
2023-05-01 17:42:50 +0600

    Create 1313. Some Words about Sport.cpp

---
+++ 1313. Some Words about Sport.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define i_mn INT_MIN
+#define i_mx INT_MAX
+
+#define vi vector<int>
+#define vll vector<ll>
+#define vb vector<bool>
+#define pb push_back
+
+#define pii pair<int, int>
+#define vip vector<pair<int, int>>
+#define m_p make_pair
+#define mii map<int, int>
+
+#define ff first
+#define ss second
+#define in insert
+#define ub upper_bound
+#define lb lower_bound
+
+#define all(v) v.begin(), v.end()
+#define asc(v) sort(v.begin(), v.end())
+#define dsc(v) sort(v.rbegin(), v.rend())
+
+#define ign cin.ignore()
+#define getln(s) getline(cin, s)
+#define fli(n) for (int i = 0; i < n; i++)
+#define rfli(n) for (int i = n - 1; i >= 0; i--)
+#define flj(n) for (int j = 0; j < n; j++)
+
+#define eps (double)(1e-7)
+#define pi (double)(acos(-1))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * b) / __gcd(a, b)
+#define mod (ll)(1e9 + 7)
+
+#define case cout << "Case " << k << ": "
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define endl "\n"
+int k;
+
+//= = = = = = = = = = End Of Template = = = = = = = = = =
+
+void solve()
+{
+    int n;
+    cin >> n;
+    int a[n][n];
+    fli(n)
+    {
+        flj(n)
+                cin >>
+            a[i][j];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        int l = i;
+        for (int j = 0; j <= i; j++)
+        {
+            cout << a[l][j] << " ";
+            l--;
+        }
+    }
+    int d = n - 1;
+    int update = 0;
+    while (d--)
+    {
+        int i = n - 1;
+        int l = i;
+        update++;
+        for (int j = update; j < n; j++)
+        {
+            cout << a[l][j] << " ";
+            l--;
+        }
+    }
+    cout << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    ll test = 1;
+    // cin >> test;
+    for (k = 1; k <= test; k++)
+    {
+        solve();
+    }
+    return 0;
+}
